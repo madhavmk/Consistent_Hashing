064c817056ed00e356ba1588d1abf4a795e74489
aishwarya <aishwaryakalgudi@gmail.com>
2019-11-29 17:27:28 +0530

    virtualization of nodes in bst implemented

--- binary_search_tree/consistent_hashing.py
+++ binary_search_tree/consistent_hashing.py
@@ -5,9 +5,10 @@ from bst import BST
 
 
 class ConsistentHasher:
-    def __init__(self):
+    def __init__(self, num_replicas):
         self.nodes = BST()
         self.id_to_node = dict()
+        self.num_replicas = num_replicas
 
     # Note: we need a deterministic hash function. Python hashlib's hash() is not deterministic, and will return
     # a different hash value on different runs.
@@ -22,15 +23,19 @@ class ConsistentHasher:
         return zlib.adler32(str.encode(item)) & 0xffffffff
 
     def add_node(self, node_name):
-        hash_id = self.hash(node_name)
-        self.nodes.insert(hash_id)
-        self.id_to_node[hash_id] = node_name
+        for i in range(1, self.num_replicas+1):
+            x = str(i) + "_" + node_name
+            hash_id = self.hash(x)
+            self.nodes.insert(hash_id)
+            self.id_to_node[hash_id] = x
 
     def remove_node(self, node_name):
-        hash_id = self.hash(node_name)
-        removed = self.nodes.remove(hash_id)
-        print('Removed %s: %r' % (node_name, removed))
-        return self.id_to_node.pop(hash_id, None)
+        for i in range(1, self.num_replicas+1):
+            x = str(i) + "_" + node_name
+            hash_id = self.hash(x)
+            removed = self.nodes.remove(hash_id)
+            print('Removed %s: %r' % (x, removed))
+            self.id_to_node.pop(hash_id, None)
 
     def assign_key_to_node(self, key):
         key_hash_id = self.hash(key)
@@ -41,6 +46,5 @@ class ConsistentHasher:
         # print('Key %s was hashed to value %d, and was assigned to node %s with id %d' % (key, key_hash_id, self.id_to_node[assigned_node_id], assigned_node_id))
         return self.id_to_node[assigned_node_id]
 
-
-
-
+    def search(self, key):
+        self.nodes.find_next_bigger_elem(key)
