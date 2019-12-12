e555feb965ee86a5a49947f746a6b356eeee6d66
aishwarya <aishwaryakalgudi@gmail.com>
2019-12-12 20:57:05 +0530

    modified

--- binary_search_tree/tests.py
+++ binary_search_tree/tests.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 
 from consistent_hashing import ConsistentHasher
 from bst import BST
@@ -6,6 +7,7 @@ from bst import BST
 class BstTest(unittest.TestCase):
 
     def test_consistent_hashing(self):
+
         keys = ["abcd", "xyza", "pqrs", "wxyz", "mnop", "defg", "gred", "ojfew", "wejfnv", "dgerfg", "ierhgj",
                 "srgergh", "ojrfi", "podjf", "powe"]
         nodes = ["A", "B", "C", "D"]
@@ -15,10 +17,17 @@ class BstTest(unittest.TestCase):
             consistent_hasher.add_node(node)
             print('\nAdded node %s \n' % (node))
 
+        start = time.time()
         for key in keys:
             node_id = consistent_hasher.assign_key_to_node(key)
-            #print('Key %s was assigned to node %s' % (key, node_id))
-            print('\nAdded node %s ' % (key))
+            print('Key %s was assigned to node %s' % (key, node_id))
+            #print('\nAdded node %s ' % (key))
+        
+        end = time.time()
+        
+        #print((end - start)/15)
+        
+        print("\n\n")
 
         node_to_remove = "B"
         consistent_hasher.remove_node(node_to_remove)
@@ -28,6 +37,22 @@ class BstTest(unittest.TestCase):
             node_id = consistent_hasher.assign_key_to_node(key)
             print('Key %s was assigned to node %s' % (key, node_id))
 
+        # print("\n\n")
+        
+        # print("defg has a hash of ", consistent_hasher.hash("defg"))
+        # print("4_B has a hash of ", consistent_hasher.hash("4_B"))
+        # print("2_C has a hash of ", consistent_hasher.hash("2_C"))
+
+        # print("\n\n")
+
+        # print("ID Node to hash mapping :  ", consistent_hasher.mapping())
+
+        # print("\n\n")
+
+        # print(" ", consistent_hasher.nodes.traverse_inorder())
+
+        # print("\n\n\n\n")
+        
         node_to_add = "B"
         consistent_hasher.add_node(node_to_add)
         print('\nAdded node %s' % (node_to_add))
