a08f1c1a015939377bc0671fb8b12ee34530e497
aishwarya <aishwaryakalgudi@gmail.com>
2019-12-12 20:56:38 +0530

    updated

--- binary_search_tree/perf_measure.py
+++ binary_search_tree/perf_measure.py
@@ -8,15 +8,24 @@ class BstTest(unittest.TestCase):
 
     def test_consistent_hashing(self):
 
-        consistent_hasher = ConsistentHasher(5)
+        n = [0, 10000, 30000, 50000, 70000, 90000]
+
+        for i in n:     
+            consistent_hasher = ConsistentHasher(5)   
+            start = time.time()
+            for node in range(1, i):
+                consistent_hasher.add_node(node)        
+            end = time.time()
+            print("n = ", i, ", \t time = ", (end - start))
         
-        start = time.time()
-        for node in range(1, 10):
-            consistent_hasher.add_node(node)
+        # print("###########\n\n")
         
-        end = time.time()
-        
-        print(end - start)
+        # consistent_hasher = ConsistentHasher(5)   
+        # for node in range(1, 2000):
+        #     start = time.time()
+        #     consistent_hasher.add_node(node)        
+        #     end = time.time()
+        #     print(end-start)
 
 if __name__ == '__main__':
     unittest.main()
