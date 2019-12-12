ca01058c33c76d71b6b6f4bf3167984e43225d0e
aishwarya <aishwaryakalgudi@gmail.com>
2019-12-12 20:56:11 +0530

    minor changes

--- binary_search_tree/consistent_hashing.py
+++ binary_search_tree/consistent_hashing.py
@@ -4,10 +4,20 @@ import zlib
 from bst import BST
 
 class ConsistentHasher:
-    def __init__(self, num_replicas):
+    def __init__(self, num_replicas, num_machines=1):
         self.nodes = BST()
         self.id_to_node = dict()
         self.num_replicas = num_replicas
+
+        for j in range(1, num_machines):
+            for i in range(1, self.num_replicas+1):
+                x = str(i) + "_" + str(j)
+                hash_id = self.hash(x)
+                self.nodes.insert(hash_id)
+                self.id_to_node[hash_id] = x
+    
+    def mapping(self):
+        return self.id_to_node
 
     # Note: we need a deterministic hash function. Python hashlib's hash() is not deterministic, and will return
     # a different hash value on different runs.
